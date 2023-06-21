9503623d262b2996a8182b6239a0f0d5724a0f69
Mulugeta Birhanu <mulugetaayelebirhanu@gmail.com>
2023-06-20 17:36:37 -0700

    alphabet

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,10 +1,13 @@
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
+#include "main.h"
+
+
+	/**
+	 * main - check the code
+	 *
+	 * Return: Always 0.
+	 */
+	int main(void)
+	{
+	    print_alphabet();
+	    return (0);
+	}
