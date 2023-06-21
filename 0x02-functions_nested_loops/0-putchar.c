2c6adc11133b43d31c97ffdc2af0e38ab88de723
Mulugeta Birhanu <mulugetaayelebirhanu@gmail.com>
2023-06-20 17:24:18 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,13 @@
 #include "main.h"
-/**
- * main - print _putchar is written by mbah
- * return: 0
- */
-int main(void)
-{
+	/**
+	 * main - print _putchar
+	 * is written by mbah
+	 * Return: 0
+	 */
+
+
+	int main(void)
+	{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -14,5 +17,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-return(0);
-}
+	return (0);
+	}
+
