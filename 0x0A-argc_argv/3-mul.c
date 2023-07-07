45b997b0562c7a4e646e5ac80d703e372e96f0a3
Mulugeta Birhanu <mulugetaayelebirhanu@gmail.com>
2023-07-07 11:22:36 -0700

    program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,12 +3,11 @@
 /**
 * _atoi - converts a string to an integer
 * @s: string to be converted
-*
 * Return: the int converted from the string
 */
 int _atoi(char *s)
 {
-int i, d, n, len, f, digit;
+	int i, d, n, len, f, digit;
 	i = 0;
 	d = 0;
 	n = 0;
@@ -17,26 +16,26 @@ int i, d, n, len, f, digit;
 	digit = 0;
 	while (s[len] != '\0')
 		len++;
-		while (i < len && f == 0)
+	while (i < len && f == 0)
 		{
 		if (s[i] == '-')
-		++d;
+			++d;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-		digit = s[i] - '0';
-		if (d % 2)
-			digit = -digit;
-		n = n * 10 + digit;
-		f = 1;
-		if (s[i + 1] < '0' || s[i + 1] > '9')
-		break;
-		f = 0;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+				f = 0;
 		}
-	i++;
-}
+		i++;
+	}
 	if (f == 0)
 		return (0);
-		return (n);
+	return (n);
 }
 /**
 * main - multiplies two numbers
@@ -47,11 +46,12 @@ int i, d, n, len, f, digit;
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
+
 	if (argc < 3 || argc > 3)
-{
-	printf("Error\n");
-	return (1);
-}
+	{
+		printf("Error\n");
+		return (1);
+	}
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
